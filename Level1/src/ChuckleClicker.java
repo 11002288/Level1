91e4c8062e016d6c72b312204833dae9c8a704e8
BigRockerEnding <bigrockerending@live.com>
2016-01-21 20:04:29 -0800

    asdf
    KZJXc

---
+++ Level1/src/ChuckleClicker.java
@@ -0,0 +1,13 @@
+
+public class ChuckleClicker {
+	public static void main(String[] args) {
+		ChuckleClicker buttons = new ChuckleClicker();
+
+		JFrame rame = new JFram();
+
+	}
+	
+	void 
+	makesbuttons() {
+	}
+}
