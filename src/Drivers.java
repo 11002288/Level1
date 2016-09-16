c0159e6834e554a4ee4095a042f8753d49b517a8
QUAGMIRE <league@WTS11.attlocal.net>
2016-09-15 19:58:36 -0700

    Net
    Flix

---
+++ src/Drivers.java
@@ -0,0 +1,12 @@
+
+public class Drivers {
+public static void main(String[] args) {
+	Burrito s = new Burrito("Eli", "Lucas");
+	s.eat();
+	s.who();
+	
+	Pokemon z = new Pokemon("Pikachu", "Eat lucas");
+	z.call();
+	z.attack();
+}
+}
---
+++ src/Platypi.java
@@ -0,0 +1,24 @@
+
+
+	public class Platypi {
+		private String name;
+String color;
+		void sayHi(){
+			System.out.println("The platypus " + name + " is smarter than your average platypus.");
+		}
+	
+		
+			
+		
+		public static void main(String[] args) {
+			//1. Make an instance of your new pet platypus
+			Platypi p = new Platypi();
+			p.name()
+			//2. Call the sayHi method
+			
+			//3. Create a constructor in the platypus class so that you can give your platypus a name.
+		}
+	}
+
+
+
