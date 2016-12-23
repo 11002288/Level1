eef0cadb8a68a12b64b0470fb0cbda582057d68d
rylr <league@WTS9.attlocal.net>
2016-12-22 19:31:54 -0800

    dfg
    adage

---
+++ src/whack.java
@@ -0,0 +1,19 @@
+import java.awt.Component;
+import java.util.Random;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class whack {
+	JFrame window = new JFrame();
+
+	public static void main(String[] args) {
+	
+
+	}
+	void drawButtons(int r){
+		Random rand = new Random();
+		
+	}
+}
